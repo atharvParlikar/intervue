cf6c304c73042f4b7fae06faaf87f1235ccca361
atharv parlikar <atharvparlikar@gmail.com>
2024-10-07 12:23:54 +0530

    ported to vanilla codemirror and got editor sync working

--- frontend/src/components/Editor.tsx
+++ frontend/src/components/Editor.tsx
@@ -1,28 +1,94 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useContext, useState } from "react";
 
-import { EditorState, basicSetup } from "@codemirror/basic-setup";
-import { EditorView, keymap } from "@codemirror/view";
+import { ChangeSpec, EditorState } from "@codemirror/state";
+import { keymap } from "@codemirror/view";
+import { EditorView, basicSetup } from "codemirror";
+import { javascript } from "@codemirror/lang-javascript";
 import { defaultKeymap, indentWithTab } from "@codemirror/commands";
+import { Button } from "@mui/material";
+import { socketContext } from "../socket";
 
-console.log(basicSetup);
-
-const Editor = () => {
+const Editor: React.FC = () => {
   const editor = useRef<null | HTMLDivElement>(null);
+  const [code, setCode] = useState("");
+  const [cursorPosition, setCursorPosition] = useState(0);
+  const socket = useContext(socketContext);
+  const viewRef = useRef<EditorView | null>(null);
+
+  const onUpdate = EditorView.updateListener.of((v) => {
+    if (
+      code !== v.state.doc.toString() ||
+      v.state.selection.main.head !== cursorPosition
+    ) {
+      setCode(v.state.doc.toString());
+      setCursorPosition(v.state.selection.main.head);
+    }
+  });
+
+  useEffect(() => {
+    if (viewRef.current && socket) {
+      socket.emit(
+        "editor-val",
+        JSON.stringify({
+          val: viewRef.current.state.doc.toString(),
+          token: localStorage.getItem("token"),
+          cursor: viewRef.current.state.selection.main.head,
+        }),
+      );
+    }
+  }, [code, cursorPosition, socket]);
 
   useEffect(() => {
     const startState = EditorState.create({
-      doc: "Hello World",
-      extensions: [keymap.of([defaultKeymap, indentWithTab])],
+      doc: "",
+      extensions: [
+        basicSetup,
+        keymap.of([defaultKeymap, indentWithTab]),
+        javascript(),
+        onUpdate,
+      ],
     });
 
-    const view = new EditorView({ state: startState, parent: editor.current });
+    if (editor.current) {
+      const view = new EditorView({
+        state: startState,
+        parent: editor.current,
+      });
+
+      viewRef.current = view;
+    }
 
     return () => {
-      view.destroy();
+      viewRef.current?.destroy();
     };
   }, []);
 
-  return <div ref={editor}></div>;
+  useEffect(() => {
+    if (socket) {
+      socket.on("editor-val", (valObject: string) => {
+        console.log("[editor-val] got valObject: ", valObject);
+        const { val, cursor } = JSON.parse(valObject);
+        viewRef.current?.dispatch({
+          changes: {
+            from: 0,
+            to: viewRef.current.state.doc.length,
+            insert: val,
+          },
+        });
+      });
+    }
+
+    return () => {
+      socket.off("editor-val");
+    };
+  }, [socket]);
+
+  return (
+    <div>
+      <div ref={editor}></div>
+      <Button onClick={() => console.log(code)}>Click me</Button>
+    </div>
+  );
 };
 
 export default Editor;
